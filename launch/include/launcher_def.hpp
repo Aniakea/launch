70430d557f42df25da84187327ca00c727defb2c
Lain <riskming@icloud.com>
2022-05-25 15:15:46 +0800

    添加了Field结构体,其中定义了一个access函数的实现 利用模板在实例化时会无视访问规则的属性 所以现在可以通过实例化Field模板和定义access函数的方式进行私有成员的访问

--- launch/include/launcher_def.hpp
+++ launch/include/launcher_def.hpp
@@ -4,12 +4,17 @@
 
 #include <type_traits>
 
-namespace launcher {
-    namespace trait {
-        template<typename B>
-        struct bundle {
-        };
-    }
-}  // namespace launcher
+namespace launcher { namespace trait {
+    template<typename B>
+    struct bundle {};
+    
+    template<typename T,
+             typename B, T B::* f>
+    struct Field {
+        friend T& access(B &instance) {
+          return instance.*f;
+        }
+    };
+}}  // namespace launcher
 
 #endif  // !LAUNCHER_DEF_HPP
